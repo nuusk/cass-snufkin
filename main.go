578e90d3b154172631e1a4f75ee4fc051f1183de
Siogop <pogod@the-hop-frog.europe-west2-c.c.bdwtest-256112.internal>
2020-02-02 16:52:09 +0000

    Intuitive and efficient client CLI

--- main.go
+++ main.go
@@ -1,44 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-
-	"github.com/gocql/gocql"
+	"strings"
+	"os"
 )
 
 func main() {
-	// connect to the cluster
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "example"
-	cluster.Consistency = gocql.Quorum
-	session, _ := cluster.CreateSession()
-	defer session.Close()
+	fmt.Println("Welcome in 5 Seconds Auctions!!!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(" ls - lists auctions\n join [id] - joins auction\n q - exit\n")
+	
+	app_state := true
 
-	// insert a tweet
-	if err := session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
-		"me", gocql.TimeUUID(), "hello world").Exec(); err != nil {
-		log.Fatal(err)
+	for app_state {
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.TrimSuffix(cmd, "\n")
+		command := strings.Split(cmd, " ")
+
+		switch command[0] {
+		case "ls":
+			fmt.Println("ls -> lists auctions")
+		case "join":
+			fmt.Println("joined auction")
+		case "q":
+			app_state = false
+		default:
+			fmt.Println("Syntax error")
+		}
+
 	}
 
-	var id gocql.UUID
-	var text string
-
-	/* Search for a specific set of records whose 'timeline' column matches
-	 * the value 'me'. The secondary index that we created earlier will be
-	 * used for optimizing the search */
-	if err := session.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
-		"me").Consistency(gocql.One).Scan(&id, &text); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Tweet:", id, text)
-
-	// list all tweets
-	iter := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`, "me").Iter()
-	for iter.Scan(&id, &text) {
-		fmt.Println("Tweet:", id, text)
-	}
-	if err := iter.Close(); err != nil {
-		log.Fatal(err)
-	}
 }
