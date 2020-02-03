57d3afbb03f7a764def9ae316f024f5080ece152
Poe <piotr@ptak.dev>
2020-02-03 11:22:15 +0000

    Wrap up exploration and deletion from pouch

--- bid.go
+++ bid.go
@@ -16,6 +16,7 @@ import (
 
 var NUM_SNUFKINS = 4
 var snufkins []string
+var items []string
 
 var cluster *gocql.ClusterConfig
 var session *gocql.Session
@@ -28,9 +29,26 @@ func initSnufkins() {
 	}
 }
 
+func initItems() {
+	items = []string{
+		"Legendary Sword Mionsz++",
+		"Ancient Belt for Loosers +2",
+		"Common Fire Bow",
+		"Rare Chest Armor",
+		"Epic Axe of Piter Bird",
+		"Distributed Ice Spell",
+	}
+}
+
 func printSnufkins() {
+	var amount float64
+
 	for i := 0; i < NUM_SNUFKINS; i++ {
-		fmt.Println(snufkins[i])
+		if err := session.Query(`SELECT SUM(amount) from transactions WHERE userId=?`, snufkins[i]).Consistency(gocql.One).Scan(&amount); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(i, snufkins[i], amount)
 	}
 }
 
@@ -115,6 +133,23 @@ func simulateUserInAuction(snufkinId string, auctionId string, itemName string, 
 	bidFinished <- true
 }
 
+func simulateUserInExhibition(snufkinId string, auctionId string, itemName string, bidFinished chan bool) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	maxRandomBidAmount := 10000
+	var simulationActive bool = isActive()
+
+	for simulationActive {
+			simulationActive = isActive()
+			bidAmount := r1.Intn(maxRandomBidAmount)
+
+			bid(auctionId, itemName, snufkinId, bidAmount)
+			fmt.Println(snufkinId, "biddig", bidAmount)
+			time.Sleep(200 * time.Millisecond)
+	}
+	bidFinished <- true
+}
+
 func endAuction(itemName string) {
 	fmt.Println("Auction ended!")
 
@@ -144,10 +179,51 @@ func endAuction(itemName string) {
 
 	transaction(winnerId, -maxBid)
 	transaction(vendorId, maxBid)
+	deleteFromPouch(snufkins[0])
+}
+
+func endExhibition(itemName string, chosenSnufkin string) {
+	fmt.Println("Exhibition ended!")
+
+	results := session.Query(`SELECT * FROM bids WHERE itemName=? ALLOW FILTERING`, itemName).Iter()
+	m := &map[string]interface{}{}
+	ret := []map[string]interface{}{}
+	var currentBid float64
+	var currentBidderId string
+
+	for results.MapScan(*m) {
+		ret := append(ret, *m)
+		m = &map[string]interface{}{}
+		currentBid, _ = strconv.ParseFloat(fmt.Sprint(ret[0]["bid"]), 64)
+		currentBidderId = fmt.Sprint(ret[0]["bidderid"])
+		transaction(currentBidderId, -currentBid)
+		transaction(chosenSnufkin, currentBid)
+	}
+
+	deleteFromPouch(snufkins[0])
+}
+
+func findItem(snufkinId string) {
+	rand.Seed(time.Now().UnixNano())
+    foundItem := items[rand.Intn(len(items))]
+	fmt.Println("Congrats! You found", foundItem)
+
+	if err := session.Query(`INSERT INTO pouches (userId, itemName, itemStartingPrice) VALUES (?, ?, ?)`,
+		snufkinId, foundItem, 0.0).Exec(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deleteFromPouch(snufkinId string) {
+	if err := session.Query(`DELETE FROM pouches WHERE id=? IF EXISTS;`,
+		snufkinId).Exec(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	initSnufkins()
+	initItems()
 	refreshFinished := make(chan bool)
 	bidFinished := make(chan bool)
 	mainUserId := snufkins[0]
@@ -176,7 +252,10 @@ func main() {
 		Selected: "\U0001F300 {{ .Description | red | cyan }}",
 	}
 
+	clearScreen()
+	
 	for {
+
 		prompt := promptui.Select{
 			Label: "What do you want to do?",
 			Items: actions,
@@ -200,8 +279,9 @@ func main() {
 			balance := getBalance(mainUserId)
 			fmt.Println("$:", balance)
 		case 1:
+			clearScreen()
 			item := getPouch(mainUserId)
-			if (item == "<empty>") {
+			if (item != "<empty>") {
 				fmt.Println("Do you want to sell:", item, "?")
 				
 				prompt := promptui.Prompt{
@@ -249,6 +329,68 @@ func main() {
 				fmt.Println("Your pouch is empty!")
 				break;
 			}
+		case 2:
+			clearScreen()
+			item := getPouch(mainUserId)
+			if (item != "<empty>") {
+				fmt.Println("Who is this exhibition for?")
+				
+				printSnufkins()
+
+				prompt := promptui.Prompt{
+					Label:  "Choose index (or c to cancel):",
+				}
+
+				result, err := prompt.Run()
+
+				if err != nil {
+					fmt.Printf("Prompt failed %v\n", err)
+					return
+				}
+
+				fmt.Println(result)
+
+				if (strings.ToLower(result) == "c") {
+					break;
+				} else {
+					chosenSnufkinIndex, _ := strconv.Atoi(result)
+					chosenSnufkin := snufkins[chosenSnufkinIndex]
+					// simulate an auction
+					fmt.Println("Simulating an exhibition...")
+
+					go func() {
+						setActive(true)
+						time.Sleep(400 * time.Millisecond)
+						setActive(false)
+					}()
+
+					go func(refreshFinished chan bool) {
+						for simulationActive {
+								refresh()
+								time.Sleep(1 * time.Second)
+						}
+						refreshFinished <- true
+					}(refreshFinished)
+
+					for i := 1; i < NUM_SNUFKINS; i++ {
+						go simulateUserInExhibition(snufkins[i], mainUserId, item, bidFinished)
+					}
+
+					<- refreshFinished
+					<- bidFinished
+					
+					endExhibition(item, chosenSnufkin)
+				}
+			} else {
+				fmt.Println("Your pouch is empty!")
+				break;
+			}
+		case 3:
+			clearScreen()
+			findItem(snufkins[0])
+		case 4:
+			clearScreen()
+			fmt.Println("Ask the developers for help")
 		case 5:
 			os.Exit(3)
 		case 6:
