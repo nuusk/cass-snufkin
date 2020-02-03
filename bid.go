f643bc8a497a7e267edeaaa640123e4b1dd9ebdd
Poe <piotr@ptak.dev>
2020-02-03 00:17:02 +0000

    Find winner bid and winner id

--- bid.go
+++ bid.go
@@ -4,16 +4,18 @@ import (
 	"log"
 	"fmt"
 	"time"
+	"math/rand"
+	"strconv"
 
 	"github.com/gocql/gocql"
+	"github.com/satori/go.uuid"
 )
 
 var cluster *gocql.ClusterConfig
 var session *gocql.Session
 
-func bid(bidAmount int) {
+func bid(bidderId string, bidAmount int) {
 	auctionId := "1"
-	bidderId := "1"
 
 	if err := session.Query(`INSERT INTO bids (auctionId, time, bid, bidderId) VALUES (?, ?, ?, ?)`,
 		auctionId, time.Now(), float64(bidAmount), bidderId).Exec(); err != nil {
@@ -33,6 +35,12 @@ func refresh() {
 
 func main() {
 	refreshFinished := make(chan bool)
+	bidFinished := make(chan bool)
+	auctionActive := true
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	maxRandomBidAmount := 10000
+
 	// connect to the cluster
 	cluster = gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "example"
@@ -40,22 +48,111 @@ func main() {
 	session, _ = cluster.CreateSession()
 	defer session.Close()
 
-	bid(4)
-	bid(3)
-	bid(7)
-	bid(1)
+
+	go func() {
+		time.Sleep(400 * time.Millisecond)
+		auctionActive = false
+	}()
 
 	go func(refreshFinished chan bool) {
-		for {
-				time.Sleep(1 * time.Second)
+		for auctionActive {
 				refresh()
+				time.Sleep(1 * time.Second)
 		}
 		refreshFinished <- true
 	}(refreshFinished)
 
-	<- refreshFinished
+	go func(bidFinished chan bool) {
+		id := uuid.Must(uuid.NewV4()).String()
 
+		for auctionActive {
+				bidAmount := r1.Intn(maxRandomBidAmount)
+
+				bid(id, bidAmount)
+				fmt.Println(id, "bidding", bidAmount)
+				time.Sleep(200 * time.Millisecond)
+		}
+		bidFinished <- true
+	}(bidFinished)
+
+	go func(bidFinished chan bool) {
+		id := uuid.Must(uuid.NewV4()).String()
+		
+		for auctionActive {
+				bidAmount := r1.Intn(maxRandomBidAmount)
+
+				bid(id, bidAmount)
+				fmt.Println(id, "bidding", bidAmount)
+				time.Sleep(200 * time.Millisecond)
+		}
+		bidFinished <- true
+	}(bidFinished)
+
+	go func(bidFinished chan bool) {
+		id := uuid.Must(uuid.NewV4()).String()
+		
+		for auctionActive {
+				bidAmount := r1.Intn(maxRandomBidAmount)
+
+				bid(id, bidAmount)
+				fmt.Println(id, "bidding", bidAmount)
+				time.Sleep(200 * time.Millisecond)
+		}
+		bidFinished <- true
+	}(bidFinished)
+
+	go func(bidFinished chan bool) {
+		id := uuid.Must(uuid.NewV4()).String()
+		
+		for auctionActive {
+				bidAmount := r1.Intn(maxRandomBidAmount)
+
+				bid(id, bidAmount)
+				fmt.Println(id, "bidding", bidAmount)
+				time.Sleep(200 * time.Millisecond)
+		}
+		bidFinished <- true
+	}(bidFinished)
+
+	<- refreshFinished
+	<- bidFinished
+
+	fmt.Println("end")
+
+	// var winnerBid float64
+	// var winnerId string
+	// if err := session.Query(`SELECT MAX(bid) from bids`).Consistency(gocql.Quorum).Scan(&winnerBid); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Max Bid:", winnerBid)
+
+	// var bidderId string
+	// var bid float64
+	results := session.Query(`SELECT * FROM bids`).Iter()
+	m := &map[string]interface{}{}
+	ret := []map[string]interface{}{}
+	// var winnerId string
+	// var winnerBid float64
+	var maxBid float64
+	var winnerId string
+	var currentBid float64
+	var currentBidderId string
+
+	for results.MapScan(*m) {
+		ret := append(ret, *m)
+		m = &map[string]interface{}{}
+		currentBid, _ = strconv.ParseFloat(fmt.Sprint(ret[0]["bid"]), 64)
+		currentBidderId = fmt.Sprint(ret[0]["bidderid"])
+		if (currentBid > maxBid) {
+			maxBid = currentBid
+			winnerId = currentBidderId
+		}
+		// fmt.Println(ret[0]["bid"])
+	}
+
+	fmt.Println(winnerId, maxBid)
 	
+
 	// var id gocql.UUID
 	// var text string
 
--- helpers/init.go
+++ helpers/init.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 
-	"github.com/gocql/gocql"
+	"github.com/gocql/gocql" 
 )
 
 var cluster *gocql.ClusterConfig
 var session *gocql.Session
 
 func initBidTable() {
-	query := "CREATE TABLE bids (auctionId text, time timestamp, bid double, bidderId text, PRIMARY KEY(auctionId, time, bidderId))"
+	query := "CREATE TABLE bids (auctionId text, time timestamp, bid double, bidderId text, PRIMARY KEY((auctionId, bidderId), time))"
 	if err := session.Query(query).Exec(); err != nil {
 		log.Fatal(err)
 	}
